a87a16915890279e702d1996444ca4a222b6d9ea
AI Couto <23392311+aicouto@users.noreply.github.com>
2019-03-01 15:44:50 -0300

    Create read.php

---
+++ read.php
@@ -0,0 +1,16 @@
+<?php
+//Criado por Anderson Ismael
+//01 de março de 2019
+
+function read($prompt = '') {
+    if ( function_exists('readline') ) {
+        $line = trim(readline($prompt));
+        if (!empty($line)) {
+            readline_add_history($line);
+        }
+    } else {
+        echo $prompt;
+        $line = trim(fgets(STDIN));
+    }
+    return $line;
+}
